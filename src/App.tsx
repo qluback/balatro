bf38aa248338de477825fb5d15027577a44b3424
Quentin LUBACK <quentin.luback@eemi.com>
2024-08-15 01:03:26 +0200

    refacto

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,13 @@
 import "./App.css";
 import { CardType } from "./types/CardType";
 import Card from "./components/Card";
-import { getPokerHand } from "./services/PokerHandChecker";
 import { CARDS_DECK_DATA } from "./balatro_data";
-import { PokerHandType } from "./types/PokerHandType";
 import useGameStore from "./stores/GameStore";
 import { useCallback } from "react";
 import ForecastPokerHand from "./components/ForecastPokerHand";
 
 function App() {
   const game = useGameStore((state) => state.game);
-  const cardsSelected = useGameStore((state) => state.cardsSelected);
-
-  function handleSelectCard(cardSelected: CardType): boolean {
-    const cardsSelectedUpdated: CardType[] = cardsSelected;
-    const existingCardSelected = cardsSelectedUpdated.findIndex(
-      (card: CardType) =>
-        card.suit === cardSelected.suit && card.label === cardSelected.label
-    );
-
-    if (existingCardSelected !== -1) {
-      cardsSelectedUpdated.splice(existingCardSelected, 1);
-    } else if (cardsSelectedUpdated.length === 5) {
-      return false;
-    } else {
-      cardsSelectedUpdated.push(cardSelected);
-    }
-
-    useGameStore.getState().setCardsSelected(cardsSelectedUpdated);
-
-    const forecastPokerHandId = getPokerHand(cardsSelected);
-    const forecastPokerHandFound: PokerHandType | undefined =
-      game.pokerHands.find(
-        (pokerHand: PokerHandType) => pokerHand.id === forecastPokerHandId
-      );
-    useGameStore
-      .getState()
-      .setForecastPokerHand(
-        forecastPokerHandFound !== undefined ? forecastPokerHandFound : null
-      );
-    
-      return true;
-  }
 
   const shuffleArray = useCallback((deck: Array<CardType>): Array<CardType> => {
     for (let i = deck.length - 1; i > 0; i--) {
@@ -68,7 +34,6 @@ function App() {
             <Card
               key={card.suit + card.label}
               card={card}
-              onSelectCard={handleSelectCard}
             />
           );
         })}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { CardType } from "../types/CardType";
+import useGameStore from "../stores/GameStore";
+import { getPokerHand } from "../services/PokerHandChecker";
+import { PokerHandType } from "../types/PokerHandType";
 
 interface CardProps {
   card: CardType;
-  onSelectCard: (card: CardType) => boolean;
 }
 
-export default function Card({card, onSelectCard}: CardProps) {
+export default function Card({ card }: CardProps) {
   const [isSelected, setIsSelected] = useState<boolean>(false);
+  const game = useGameStore((state) => state.game);
+  const cardsSelected = useGameStore((state) => state.cardsSelected);
 
   let cssClasses = "border h-36 flex flex-col justify-center items-center";
 
@@ -15,23 +19,41 @@ export default function Card({card, onSelectCard}: CardProps) {
     cssClasses += " bg-red-200";
   }
 
-  function handleClick(card: CardType) {
-    const result = onSelectCard({suit: card.suit, label: card.label, order: card.order, points: card.points});
-    console.log(result)
-    if (result) {
-      setIsSelected(prevState => !prevState);
+  function handleSelectCard(cardSelected: CardType) {
+    const cardsSelectedUpdated: CardType[] = cardsSelected;
+    const existingCardSelected = cardsSelectedUpdated.findIndex(
+      (card: CardType) =>
+        card.suit === cardSelected.suit && card.label === cardSelected.label
+    );
+
+    if (existingCardSelected !== -1) {
+      cardsSelectedUpdated.splice(existingCardSelected, 1);
+    } else if (cardsSelectedUpdated.length === 5) {
+      return;
+    } else {
+      cardsSelectedUpdated.push(cardSelected);
     }
-  } 
+
+    useGameStore.getState().setCardsSelected(cardsSelectedUpdated);
+
+    const forecastPokerHandId = getPokerHand(cardsSelected);
+    const forecastPokerHandFound: PokerHandType | undefined =
+      game.pokerHands.find(
+        (pokerHand: PokerHandType) => pokerHand.id === forecastPokerHandId
+      );
+    useGameStore
+      .getState()
+      .setForecastPokerHand(
+        forecastPokerHandFound !== undefined ? forecastPokerHandFound : null
+      );
+
+    setIsSelected((prevState) => !prevState);
+  }
 
   return (
-    <button
-      className={cssClasses}
-      onClick={() => handleClick(card)}
-    >
+    <button className={cssClasses} onClick={() => handleSelectCard(card)}>
       <p>{card.suit}</p>
-      <p>
-        {card.label}
-      </p>
+      <p>{card.label}</p>
     </button>
   );
 }
